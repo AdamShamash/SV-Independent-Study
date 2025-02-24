8b3fb27b79e6ca1ff8db4312b6e7b6b9a6311e86
AdamShamash <ashamash@seas.upenn.edu>
2025-02-24 16:05:31 -0500

    added pll module

--- TestVerilog.sv
+++ TestVerilog.sv
@@ -1,3 +1,67 @@
+module SystemConnect (
+    input wire external_clk_25MHz,
+    output wire [7:0] led,
+    inout wire gpdi_sda,
+    output wire gpdi_scl
+);
+
+wire ignore;
+
+I2C_main instance1 (.sda_i(gpdi_sda), .sda_o(gpdi_sda), .scl_o(ignore))
+
+endmodule
+
+module pll
+(
+    input input_clk_25MHz, // 25 MHz, 0 deg
+    output clk_4x, // 6.25 MHz, 0 deg
+    output clkout1, // 3.20856 MHz, 0 deg
+    output locked
+);
+wire clkfb;
+(* FREQUENCY_PIN_CLKI="25" *)
+(* FREQUENCY_PIN_CLKOP="6.25" *)
+(* FREQUENCY_PIN_CLKOS="3.20856" *)
+(* ICP_CURRENT="12" *) (* LPF_RESISTOR="8" *) (* MFG_ENABLE_FILTEROPAMP="1" *) (* MFG_GMCREF_SEL="2" *)
+EHXPLLL #(
+        .PLLRST_ENA("DISABLED"),
+        .INTFB_WAKE("DISABLED"),
+        .STDBY_ENABLE("DISABLED"),
+        .DPHASE_SOURCE("DISABLED"),
+        .OUTDIVIDER_MUXA("DIVA"),
+        .OUTDIVIDER_MUXB("DIVB"),
+        .OUTDIVIDER_MUXC("DIVC"),
+        .OUTDIVIDER_MUXD("DIVD"),
+        .CLKI_DIV(4),
+        .CLKOP_ENABLE("ENABLED"),
+        .CLKOP_DIV(96),
+        .CLKOP_CPHASE(47),
+        .CLKOP_FPHASE(0),
+        .CLKOS_ENABLE("ENABLED"),
+        .CLKOS_DIV(187),
+        .CLKOS_CPHASE(47),
+        .CLKOS_FPHASE(0),
+        .FEEDBK_PATH("INT_OP"),
+        .CLKFB_DIV(1)
+    ) pll_i (
+        .RST(1'b0),
+        .STDBY(1'b0),
+        .CLKI(input_clk_25MHz),
+        .CLKOP(clk_4x),
+        .CLKOS(clkout1),
+        .CLKFB(clkfb),
+        .CLKINTFB(clkfb),
+        .PHASESEL0(1'b0),
+        .PHASESEL1(1'b0),
+        .PHASEDIR(1'b1),
+        .PHASESTEP(1'b1),
+        .PHASELOADREG(1'b1),
+        .PLLWAKESYNC(1'b0),
+        .ENCLKOP(1'b0),
+        .LOCK(locked)
+        );
+endmodule
+
 module I2C_main (
     input  wire sda_i,
     output logic sda_o,
